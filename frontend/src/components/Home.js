abbc2e8e1fc23c8420fed74de050abf8a7f37bdd
RoryHemmings <roryhemmings@ucla.edu>
2023-04-22 23:06:28 -0700

    add models backend funcionality

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Mapcontainer from "./Mapcontainer";
 import Results from "./Results";
 import '../styles/Home.css';
@@ -6,11 +6,23 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 
+
+
 export default function Home() {
   const [isStart, setisStart] = useState(true);
   const [startPos, setStartPos] = useState({ lat: 0, lng: 0 });
   const [endPos, setEndPos] = useState({ lat: 0, lng: 0 });
-  const [results, setResults] = useState();
+  const [models, setModels] = useState([]);
+  const [results, setResults] = useState(undefined);
+
+  /* fetch list of car models */
+  useEffect(() => {
+    fetch('http://localhost:5000/models')
+      .then(res => res.json())
+      .then(data => {
+        setModels(data.models);
+      });
+  }, []);
 
   const handleButton = () => {
     let ljson = JSON.stringify(startPos)
@@ -37,23 +49,23 @@ export default function Home() {
     <div className="home">
       <div className="header">
       </div>
-            <div className="car">
-                <h className="car-title">
-                    Vehicle model
-                </h>
-                    <Autocomplete
-                        disablePortal
-                        id="model-selector"
-                        /*options={}*/
-                        sx={{ width: 300 }}
-                        renderInput={(params) => <TextField {...params} label="Movie" />}
-                    />
-            </div>
-            <div className='map'>
-                <Mapcontainer handleClick={handleClick} startPos={startPos} endPos={endPos}/>
-                <button className='button' onClick={handleButton}>Click here to submit</button>
-            </div>
-            <Results results={results} />
-        </div>
-    )
+      <div className="car">
+        <h className="car-title">
+          Vehicle model
+        </h>
+        <Autocomplete
+          disablePortal
+          id="model-selector"
+          options={models}
+          sx={{ width: 300 }}
+          renderInput={(params) => <TextField {...params} model='model' />}
+        />
+      </div>
+      <div className='map'>
+        <Mapcontainer handleClick={handleClick} startPos={startPos} endPos={endPos} />
+        <button className='button' onClick={handleButton}>Click here to submit</button>
+      </div>
+      {results != undefined && <Results results={results} />}
+    </div>
+  )
 }
