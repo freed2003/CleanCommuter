874ea604e5175bd9c163cdb980049201cd546900
RoryHemmings <roryhemmings@ucla.edu>
2023-04-22 17:05:10 -0700

    almost complete google maps code

--- backend/utils/maps.js
+++ backend/utils/maps.js
@@ -1,23 +1,32 @@
+require('dotenv').config();
+
 const getMapsData = async (start, end) => {
+  const mapsEndpoint = 'https://maps.googleapis.com/maps/api/directions/json';
+  const dest = `destination=${start.lat},${start.lng}`;
+  const origin = `origin=${end.lat},${end.lng}`;
+  const key = `key=${process.env.MAPS_API_KEY}`;
+  // const methods = ['driving', 'walking', 'bicycling', 'transit'];
+  const methods = ['driving', 'walking', 'bicycling'];
 
+  /* request each method in parallel */
+  const requests = methods.map(method => fetch(
+    `${mapsEndpoint}?${dest}&${origin}&${key}&mode=${method}`
+  ));
+  const responses = await Promise.all(requests);
+  const json = responses.map((response => response.json()));
+  const data = await Promise.all(json);
+  console.log(data);
 
-  return [
-    {
-      method: 'DRIVE',
-      distance: 10,
-      travel_time: 30,
-    },
-    {
-      method: 'WALK',
-      distance: 10,
-      travel_time: 80,
-    },
-    {
-      method: 'BICYCLE',
-      distance: 10,
-      travel_time: 40,
-    },
-  ];
+  let ret = [];
+  data.forEach(method => {
+    ret.push({
+      method: method,
+      distance: data.routes[0].legs.distance.value / 1000, // distance in km
+      travel_time: data.routes[0].legs.duration.value / 60, // time in minutes
+    });
+  });
+
+  return ret;
 };
 
 module.exports = {
